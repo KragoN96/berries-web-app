fb1c4dcb826e4a0a36eeaea92903023fecbf934c
kp-aris-dascalu <aris.dascalu@kape.com>
2025-12-12 19:39:54 +0200

    [WEB-12] Deleted two elements from the first import line from the react file, added them back

--- lost_and_found/src/pages/home-page-main.jsx
+++ lost_and_found/src/pages/home-page-main.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useRef, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import '../styles/CSS/home_page_main.css';
 import landingIllustration from '../Pictures/IllustrationPack/PNG/landing_page_illustration.png';

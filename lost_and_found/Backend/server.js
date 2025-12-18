cf8667ce6e05264673b10b610b1fa39d1d3e8506
Madalin Savu <savu.madalin26@gmial.com>
2025-12-18 18:35:31 +0200

    [WEB-24]postari-comentarii-myaccount-feed-conflicterezolvate

--- lost_and_found/Backend/server.js
+++ lost_and_found/Backend/server.js
@@ -8,15 +8,25 @@ const bcrypt = require("bcrypt");
 const itemsRoutes = require("./lost_items/items.routes");
 const uploadsRoutes = require("./uploads.routes");
 const requireAuth = require("./middleware/requireAuth");
+const crypto = require("crypto");
 
 
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 // MongoDB connection
-const MONGO_URI = "mongodb://localhost:27017/";
-const DB_NAME = "ip_location";
+require("dotenv").config();
+
+// MongoDB connection
+const MONGO_URI = process.env.MONGO_URI;
+const DB_NAME = process.env.DB_NAME || "ip_location";
+
+if (!MONGO_URI) {
+  console.error("Missing MONGO_URI env var");
+  process.exit(1);
+}
+
 const TRACKING_COLLECTION = "ip_locations"; // pentru IP tracking
 const USERS_COLLECTION = "users_info";      // pentru utilizatori (email + passwordHash)
 
@@ -442,10 +452,6 @@ app.post("/api/auth/reset-password", async (req, res) => {
     return res.status(500).json({ message: "Eroare server." });
   }
 });
-
-
-
-const crypto = require("crypto");
 //import { sendEmail } from "./sendEmail.js";
 
 const User = require("./User");
